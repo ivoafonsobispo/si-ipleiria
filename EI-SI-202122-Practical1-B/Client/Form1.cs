e72ffee1875738903b02acb46402ad7092f9bee9
Ivo Afonso Bispo <ivoafonsobispo@protonmail.com>
2022-04-23 13:09:12 +0100

    Pratical Exam 1

---
+++ EI-SI-202122-Practical1-B/Client/Form1.cs
@@ -0,0 +1,158 @@
+﻿using EI.SI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class FormEISIPracticalExam : Form
+    {
+        // Assimetric Keys
+        RSACryptoServiceProvider rsaClient = null;
+        RSACryptoServiceProvider rsaServer = null;
+
+        // Symetric
+        AesCryptoServiceProvider aes = null;
+        SymmetricsSI symmetricsSI = null;
+
+        // Comunication
+        IPEndPoint serverEndPoint;
+        TcpClient client = null;
+        ProtocolSI protocol = null;
+        NetworkStream netStream = null;
+
+        SHA256CryptoServiceProvider sha256 = null;
+
+        byte[] msg = null;
+
+        public FormEISIPracticalExam()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGenerateClientAssymetricalKeys_Click(object sender, EventArgs e)
+        {
+            // Assimetric Client Algorithm
+            rsaClient = new RSACryptoServiceProvider();
+            MessageBox.Show("Client Assymetric Key Has Been Generated");
+        }
+
+        private void buttonGenerateRecipientAsymmetricalKeys_Click(object sender, EventArgs e)
+        {
+            // Assimetric Recipient Algorithm
+            rsaServer = new RSACryptoServiceProvider();
+            MessageBox.Show("Recepient Assymetric Key Has Been Generated");
+        }
+
+        private void buttonGenerateSymmetricalElementsRecipient_Click(object sender, EventArgs e)
+        {
+            // Symmetric Algorithm
+            aes = new AesCryptoServiceProvider();
+            symmetricsSI = new SymmetricsSI(aes);
+            MessageBox.Show("Symmetric Algorithm Has Been Generated");
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (textBoxServerIPAddress.Text.Length == 0 || textBoxServerPort.Text.Length == 0)
+            {
+                MessageBox.Show("Please fill both fields");
+                return;
+            }
+
+            //--- IP
+            string ip = textBoxServerIPAddress.Text;
+            //--- Porto
+            int port = Int32.Parse(textBoxServerPort.Text);
+
+            // Client/Server Protocol to SI
+            protocol = new ProtocolSI();
+
+            // Defenitions for TcpClient: IP:port (127.0.0.1:9999)
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            // Connects to Server ...
+            client = new TcpClient();
+            client.Connect(serverEndPoint);
+            netStream = client.GetStream();
+
+            MessageBox.Show("Connected to the server with success");
+        }
+
+        private void buttonExchangeAsymmetricalKeys_Click(object sender, EventArgs e)
+        {
+            #region Exchange Public Keys
+            msg = protocol.Make(ProtocolSICmdType.PUBLIC_KEY, rsaClient.ToXmlString(false));
+            netStream.Write(msg, 0, msg.Length);
+
+            netStream.Read(protocol.Buffer, 0, protocol.Buffer.Length);
+            rsaServer.FromXmlString(protocol.GetStringFromData());
+            MessageBox.Show("Public Key shared");
+            #endregion
+        }
+
+        private void buttonSendSymmetricElementsRecipient_Click(object sender, EventArgs e)
+        {
+            // Hash Algorithm
+            sha256 = new SHA256CryptoServiceProvider();
+
+            // Send key...
+            byte[] key = rsaServer.Encrypt(aes.Key, true);
+            byte[] encryptedKey = protocol.Make(ProtocolSICmdType.SECRET_KEY, key);
+            netStream.Write(encryptedKey, 0, encryptedKey.Length);
+
+
+            // Receive the cipher
+            netStream.Read(protocol.Buffer, 0, protocol.Buffer.Length);
+            byte[] signature = protocol.GetData();
+
+            if (rsaServer.VerifyData(key, sha256, signature))
+            {
+                MessageBox.Show("Signature Valid");
+            }
+            else
+            {
+                MessageBox.Show("Signature Invalid");
+            }
+        }
+
+        private void buttonEncryptSendMessageRecipient_Click(object sender, EventArgs e)
+        {
+            // Hash Algorithm
+            sha256 = new SHA256CryptoServiceProvider();
+
+            // Send data...
+            byte[] clearData = Encoding.UTF8.GetBytes(textBoxMessageToSend.Text);
+            byte[] encryptedData = symmetricsSI.Encrypt(clearData);
+            msg = protocol.Make(ProtocolSICmdType.DATA, encryptedData);
+            netStream.Write(msg, 0, msg.Length);
+
+            // Receive the cipher
+            netStream.Read(protocol.Buffer, 0, protocol.Buffer.Length);
+            byte[] signature = protocol.GetData();
+
+            if (rsaServer.VerifyData(encryptedData, sha256, signature))
+            {
+                MessageBox.Show("Signature Valid");
+            }
+            else
+            {
+                MessageBox.Show("Signature Invalid");
+            }
+        }
+    }
+}
---
+++ EI-SI-202122-Practical1-B/Server/Server.cs
@@ -0,0 +1,104 @@
+﻿using EI.SI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Server
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint listenEndPoint;
+            TcpListener listener = null;
+            TcpClient client = null;
+            NetworkStream networkStream = null;
+            ProtocolSI protocol = null;
+            RSACryptoServiceProvider rsaClient = null;
+            RSACryptoServiceProvider rsaServer = null;
+            SHA256CryptoServiceProvider sha256 = null;
+
+            try
+            {
+                Console.WriteLine($"** SERVER: Practical Exam on {DateTime.Today.ToLongDateString()} - Version B **");
+
+                listenEndPoint = new IPEndPoint(IPAddress.Any, 10000);
+                listener = new TcpListener(listenEndPoint);
+
+                Console.Write("Waiting for client... ");
+                listener.Start();
+                client = listener.AcceptTcpClient();
+                networkStream = client.GetStream();
+                Console.WriteLine("OK.");
+
+                protocol = new ProtocolSI();
+                byte[] ack = protocol.Make(ProtocolSICmdType.ACK);
+                byte[] msg = null;
+
+                rsaServer = new RSACryptoServiceProvider();
+                rsaClient = new RSACryptoServiceProvider();
+                sha256 = new SHA256CryptoServiceProvider();
+
+                Console.Write("Reading Public Key... ");
+                networkStream.Read(protocol.Buffer, 0, protocol.Buffer.Length);
+                Console.WriteLine("OK.");
+                String clientPublicKey = protocol.GetStringFromData();
+                byte[] packet = protocol.Make(ProtocolSICmdType.PUBLIC_KEY, rsaServer.ToXmlString(false));
+                Console.WriteLine("Sending Public Key... OK.");
+                networkStream.Write(packet, 0, packet.Length);
+
+
+                Console.Write("Reading Recipient Symmetric Elements... ");
+                networkStream.Read(protocol.Buffer, 0, protocol.Buffer.Length);
+                byte[] encryptedSymKey = protocol.GetData();
+                Console.WriteLine("OK.");
+
+                Console.WriteLine("Signing and sending....");
+                byte[] signature = rsaServer.SignData(encryptedSymKey, sha256);
+                msg = protocol.Make(ProtocolSICmdType.DIGITAL_SIGNATURE, signature);
+                networkStream.Write(msg, 0, msg.Length);
+
+                Console.Write("Reading Recipient Message...");
+                networkStream.Read(protocol.Buffer, 0, protocol.Buffer.Length);
+                byte[] encryptedMessage = protocol.GetData();
+                Console.WriteLine("OK.");
+
+                Console.WriteLine("Signing and sending....");
+                signature = rsaServer.SignData(encryptedMessage, sha256);
+                msg = protocol.Make(ProtocolSICmdType.DIGITAL_SIGNATURE, signature);
+                networkStream.Write(msg, 0, msg.Length);
+                Console.WriteLine("OK.");
+
+
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Error: {exception.ToString()}");
+            } finally
+            {
+                if (sha256 != null)
+                    sha256.Dispose();
+                if (rsaClient != null)
+                    rsaClient.Dispose();
+                if (rsaServer != null)
+                    rsaServer.Dispose();
+                if (networkStream != null)
+                    networkStream.Dispose();
+                if (client != null)
+                    client.Close();
+                if (listener != null)
+                    listener.Stop();
+            }
+
+            Console.Write("End: Press a key...");
+            Console.ReadKey();
+
+        } // main
+    } // class
+} // namaspace
